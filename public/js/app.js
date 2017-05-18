2eb62e9591dc85a398af0a6fc2171f9ea58f0ef4
Muzammil Hussain <cmuzammil786@gmail.com>
2017-05-18 11:56:19 +0530

    chat app

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,19 @@
 var socket = io();
 
 socket.on('connect', function(){
-	console.log('connected to socket.io server ')
+	console.log('connected to socket.io server ');
 });
+
+
+socket.on('message',function (message){
+	console.log('new message');
+	console.log(message.text);
+
+
+});
+socket.on('message', function (message){
+		console.log('message recieved' + message.txt);
+
+		// io.emit
+		socket.broadcast.emit('message', message)
+	});
